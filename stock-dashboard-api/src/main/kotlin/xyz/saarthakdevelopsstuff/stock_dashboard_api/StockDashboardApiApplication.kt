6d2061d2e626a1a9e2ba1a80aa86268afa228a64
Saarthak Verma <saarthak_verma@intuit.com>
2025-12-21 12:14:06 +0530

    scaffold

---
+++ stock-dashboard-api/src/main/kotlin/xyz/saarthakdevelopsstuff/stock_dashboard_api/StockDashboardApiApplication.kt
@@ -0,0 +1,11 @@
+package xyz.saarthakdevelopsstuff.stock_dashboard_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StockDashboardApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<StockDashboardApiApplication>(*args)
+}
---
+++ stock-dashboard-api/src/test/kotlin/xyz/saarthakdevelopsstuff/stock_dashboard_api/StockDashboardApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package xyz.saarthakdevelopsstuff.stock_dashboard_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StockDashboardApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
