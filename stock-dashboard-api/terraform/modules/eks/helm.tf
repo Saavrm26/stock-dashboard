f063e44c9f69697ac41c52b128769c56ae24b096
Saarthak Verma <saarthak_verma@intuit.com>
2025-12-31 13:59:29 +0530

    load balancer controller

--- stock-dashboard-api/terraform/modules/eks/helm.tf
+++ stock-dashboard-api/terraform/modules/eks/helm.tf
@@ -30,8 +30,35 @@ resource "helm_release" "external_dns" {
 # todo: add load balancer controller
 
 resource "helm_release" "aws_load_balancer_controller" {
-  chart = ""
-  name  = ""
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  name       = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+
+  set = [
+    {
+      name  = "clusterName"
+      value = module.eks.cluster_name
+    },
+    {
+      name  = "serviceAccount.create"
+      value = "true"
+    },
+    {
+      name  = "serviceAccount.name"
+      value = "aws-load-balancer-controller"
+    },
+    {
+      name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+      value = aws_iam_role.aws_load_balancer_controller.arn
+    },
+    {
+      name  = "vpcId"
+      value = var.vpc_id
+    }
+  ]
+
+  depends_on = [module.eks, module.spot_eks_managed_node_group]
 }
 
 # todo: add cluster-autoscaler
--- stock-dashboard-api/terraform/modules/eks/roles.tf
+++ stock-dashboard-api/terraform/modules/eks/roles.tf
@@ -82,3 +82,37 @@ resource "aws_iam_role_policy_attachment" "external_dns" {
   role       = aws_iam_role.external_dns.name
   policy_arn = aws_iam_policy.external_dns.arn
 }
+
+# IRSA for AWS Load Balancer Controller
+resource "aws_iam_role" "aws_load_balancer_controller" {
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = module.eks.oidc_provider_arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "${module.eks.oidc_provider}:aud" = "sts.amazonaws.com"
+            "${module.eks.oidc_provider}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
+          }
+        }
+      }
+    ]
+  })
+}
+
+# curl -O https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.14.1/docs/install/iam_policy.json
+resource "aws_iam_policy" "aws_load_balancer_controller" {
+  name        = "AWSLoadBalancerControllerIAMPolicy_${var.cluster_name}"
+  description = "Policy to allow k8s load balancer controller to manage AWS ELB"
+  policy = file("${path.module}/aws-load-balancer-controller-policy.json")
+}
+
+resource "aws_iam_role_policy_attachment" "aws_load_balancer_controller" {
+  role = aws_iam_role.aws_load_balancer_controller.name
+  policy_arn = aws_iam_policy.aws_load_balancer_controller.arn
+}
