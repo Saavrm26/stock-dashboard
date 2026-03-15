72f5a36934101b796b4d3c6908eaeeb8986f98d5
Saarthak Verma <saarthakverma739@saarthakdevelopsstuff.xyz>
2026-03-15 17:30:47 +0530

    remove autoreload

--- stock-dashboard-api-sidecar/main.py
+++ stock-dashboard-api-sidecar/main.py
@@ -1,18 +1,19 @@
 import uvicorn
 from fastapi import FastAPI
+import os
 
 from app.controllers.stocks_controller import router as stocks_router
 
 app = FastAPI()
 app.include_router(stocks_router)
-
+app_env = os.getenv("APP_ENV", "development")
 
 def main():
     uvicorn.run(
         "main:app",
         host="127.0.0.1",
         port=8081,
-        reload=True,
+        reload=True if app_env == "development" else False,
     )
 
 
